e317e180758f9b3510b006a48af616a8c5fd7816
chrisaor <pchpch0070@gmail.com>
2018-07-04 15:58:33 +0900

    bootstrap 적용

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from blog.models import Post
+
+
 def post_list(request):
-    return HttpResponse('First deploy')
+    posts = Post.objects.all()
+    context = {
+        'posts':posts,
+    }
+    return render(request, 'post/post_list.html', context)
